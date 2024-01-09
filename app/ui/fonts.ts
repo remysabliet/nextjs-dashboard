9165d751bf4681ff0da261a02691c2878739afc6
Rémy Sabliet <sabliet@gmail.com>
2024-01-09 09:32:42 +0900

    CSS, font and image

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana, Nunito } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '400' });
+
+export const nunito = Nunito({ subsets: ['latin'], weight: '200' });
