de347f71d2dd1f1fba653846dd93b463bddfdc7c
Rémy Sabliet <sabliet@gmail.com>
2024-01-13 10:46:43 +0900

    Chap 13: error handling

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,5 +1,6 @@
 import { sql } from '@vercel/postgres';
 import { unstable_noStore as noStore } from 'next/cache';
+import { notFound } from 'next/navigation';
 
 import {
   CustomerField,
@@ -151,8 +152,8 @@ export async function fetchInvoicesPages(query: string) {
 
 export async function fetchInvoiceById(id: string) {
   noStore();
-  try {
-    const data = await sql<InvoiceForm>`
+
+  const data = await sql<InvoiceForm>`
       SELECT
         invoices.id,
         invoices.customer_id,
@@ -162,17 +163,18 @@ export async function fetchInvoiceById(id: string) {
       WHERE invoices.id = ${id};
     `;
 
-    const invoice = data.rows.map((invoice) => ({
-      ...invoice,
-      // Convert amount from cents to dollars
-      amount: invoice.amount / 100,
-    }));
+  const invoice = data.rows.map((invoice) => ({
+    ...invoice,
+    // Convert amount from cents to dollars
+    amount: invoice.amount / 100,
+  }));
 
-    return invoice[0];
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch invoice.');
+  console.log('before', invoice[0]);
+  if (!invoice[0]) {
+    notFound(); // 404 error handling
   }
+
+  return invoice[0];
 }
 
 export async function fetchCustomers() {
