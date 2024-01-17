a324b41f664e98695b95a94bdf0b94763ddae764
Rémy Sabliet <sabliet@gmail.com>
2024-01-17 12:46:30 +0900

    Chap 16: Metadata

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import { lusitana } from '@/app/ui/fonts';
 import {
@@ -61,6 +61,7 @@ export default function LoginForm() {
               <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
+          Email: user@nextmail.com Password: 123456
         </div>
         <LoginButton />
         <div
@@ -82,7 +83,7 @@ export default function LoginForm() {
 
 function LoginButton() {
   const { pending } = useFormStatus();
- 
+
   return (
     <Button className="mt-4 w-full" aria-disabled={pending}>
       Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
